93d80e2ead61c2c427fca4f43ec4bfb22adcde56
Vermouth117 <e153jsb7eg@gmail.com>
2023-09-21 02:14:54 +0900

    Update: Home-page.tsx, Home-page.module.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 
-import Header from "./components/organisms/Header";
 import styles from "./page.module.css";
+import Header from "./components/organisms/Header";
+import Footer from "./components/organisms/Footer";
 
 export default function Home() {
   return (
     <div className={styles.home}>
       <Header />
+      <main>
+        <div className={styles.homeImage}></div>
+      </main>
+      <Footer />
     </div>
   );
 };
